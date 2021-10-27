27949bef94f8675bfbe058ff1d7be616381227a6
João Leonardo <joaolhd18@gmail.com>
2021-10-26 22:11:14 -0300

    WebSystem  - Criado projeto.  - Criado estrutura de pastas.  - Desenvolvido Service genérico para CRUD .

---
+++ src/app/pages/usuario/usuario.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+// shared
+import { CrudService } from 'src/app/shared/components/crud-service/crud.service';
+
+// aplicação
+import { Usuario } from './models/usuario';
+
+@Injectable()
+export class UsuarioService extends CrudService<Usuario> {
+
+    constructor(public http: HttpClient) {
+        super('usuario', http);
+    }
+
+}
---
+++ src/app/shared/components/crud-service/crud.service.ts
@@ -0,0 +1,57 @@
+import { Inject, Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http"
+import { Observable } from "rxjs";
+
+@Injectable()
+export abstract class CrudService<T> {
+
+    constructor(
+        @Inject('url') public url: string,
+        public http: HttpClient,
+    ) { }
+
+    /**
+     * @description Inclui o novo registro
+     * @returns Void
+     * @param novo Registro da inclusão
+     */
+    public incluir(novo: T): Observable<void> {
+        return this.http.post<void>(this.url + '/incluir', novo);
+    }
+
+    /**
+     * @description Atualiza o registro
+     * @returns Void
+     * @param registro Registro da atualização
+     */
+    public atualizar(registro: T): Observable<void> {
+        return this.http.put<void>(this.url + '/atualizar', registro);
+    }
+
+    /**
+     * @description Pesquisa todos os registros do objeto
+     * @returns Vetor com todos os registro do objeto
+     */
+    public pesquisarTodos(): Observable<T[]> {
+        return this.http.get<T[]>(this.url + '/pesquisar-todos');
+    }
+
+    /**
+     * @description Busca o registro pelo id
+     * @returns Registro correspondente ao id (ou null)
+     * @param id Identificador do registro
+     */
+    public carregar(id: number): Observable<T> {
+        return this.http.get<T>(this.url + '/' + id);
+    }
+
+    /**
+     * @description Deleta o registro pelo id
+     * @returns Void
+     * @param id Identificador do registro
+     */
+    public remover(id: number): Observable<void> {
+        return this.http.delete<void>(this.url + '/' + id);
+    }
+
+}
