11672f38ca9defcb8e28392f38930aa092dad750
João Leonardo <joaolhd18@gmail.com>
2021-11-02 18:57:46 -0300

    Usuario (dialog)  - Montado form com stepper;  - Implementado tema customizado (scss) para o sistema;

--- src/app/pages/usuario/modules/usuario-dialog/usuario-dialog.component.ts
+++ src/app/pages/usuario/modules/usuario-dialog/usuario-dialog.component.ts
@@ -1,16 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-// material
-import { MatDialog } from '@angular/material/dialog';
+// aplicação
+import { UsuarioService } from '../../usuario.service';
 
 @Component({
     selector: 'app-usuario-dialog',
-    templateUrl: 'usuario-dialog.component.html'
+    templateUrl: 'usuario-dialog.component.html',
+    styleUrls: ['./usuario-dialog.component.scss'],
+    providers: [
+        // UsuarioService
+    ]
 })
 export class UsuarioDialogComponent implements OnInit {
 
-    constructor(public dialog: MatDialog) { }
+    /**
+     * @description FormGroup da modal 
+     */
+    public form: FormGroup;
+
+    /**
+     * @description Label do botão de fechamento da modal
+     */
+    public buttonLabel: 'Cancelar' | 'Finalizar';
+
+    constructor(
+        // private usuarioService: UsuarioService,
+        private formBuilder: FormBuilder,
+    ) {
+        // inicializa o form
+        this.form = this.criarForm();
+
+        // incializa as variáveis do template
+        this.buttonLabel = 'Cancelar';
+    }
 
     ngOnInit() { }
+
+    public get dadosPessoaisForm(): FormGroup {
+        return this.form.controls['dadosPessoais'] as FormGroup;
+    }
+
+    public get dadosUsuarioForm(): FormGroup {
+        return this.form.controls['dadosUsuario'] as FormGroup;
+    }
+
+    private criarForm(): FormGroup {
+        return this.formBuilder.group({
+            dadosPessoais: this.formBuilder.group({
+                nome: [null, Validators.required],
+                sobrenome: [null, Validators.required],
+                email: [null, Validators.required],
+            }),
+            dadosUsuario: this.formBuilder.group({
+                username: [null, Validators.required],
+                password: [null, Validators.required],
+                confirmacaoPassword: [null, Validators.required],
+            }),
+        });
+    }
+
+    /**
+     * @description Executa no click do botão "Cadastrar" do stepper
+     * * Valida o form e cadastra o usuário
+     */
+    public onClickBtnCadastrar() {
+        this.form.updateValueAndValidity();
+
+        if (this.form.valid) {
+            // TODO: cadastrar o usuário
+            this.buttonLabel = 'Finalizar';
+        }
+    }
 
 }
--- src/app/pages/usuario/modules/usuario-dialog/usuario-dialog.module.ts
+++ src/app/pages/usuario/modules/usuario-dialog/usuario-dialog.module.ts
@@ -1,12 +1,14 @@
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 // material
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 // aplicação
 import { UsuarioDialogComponent } from './usuario-dialog.component';
@@ -15,12 +17,15 @@ import { UsuarioDialogComponent } from './usuario-dialog.component';
     imports: [
         CommonModule,
         FormsModule,
+        BrowserModule,
+        ReactiveFormsModule,
 
         // material
         MatStepperModule,
         MatButtonModule,
         MatDialogModule,
         MatInputModule,
+        MatIconModule,
     ],
     exports: [UsuarioDialogComponent],
     declarations: [UsuarioDialogComponent],
