565b3e44d6777712300737ffed4727899cc5e176
João Leonardo <joaolhd18@gmail.com>
2021-11-06 11:49:43 -0300

    CRUD  - Adicionado controle de rotas.

--- src/app/shared/components/crud/crud-component.ts
+++ src/app/shared/components/crud/crud-component.ts
@@ -1,4 +1,6 @@
+import { Injectable, OnInit } from "@angular/core";
 import { FormGroup } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
 
 // material
 import { MatSnackBar } from "@angular/material/snack-bar";
@@ -6,7 +8,8 @@ import { MatSnackBar } from "@angular/material/snack-bar";
 // aplicação
 import { CrudService } from "./crud.service";
 
-export abstract class CrudComponent<T> {
+@Injectable()
+export abstract class CrudComponent<T> implements OnInit {
 
     /**
      * @description Formulário do componente
@@ -21,15 +24,40 @@ export abstract class CrudComponent<T> {
     constructor(
         public service: CrudService<T>,
         public snackBar: MatSnackBar,
+        public route: ActivatedRoute,
     ) {
         this.form = this.criarForm();
         this.loading = false;
+    }
+
+    ngOnInit(): void {
+        this.verificarCrudRouteParams();
     }
 
     /**
      * @description Cria o form do CRUD
      */
     public abstract criarForm(): FormGroup;
+
+    /**
+     * @description Retorna um novo registro (T)
+     */
+    public abstract get novoRegistro(): T;
+
+    /**
+     * @description Cria o form do CRUD
+     */
+    public verificarCrudRouteParams() {
+        this.route.params.subscribe(params => {
+            if (params['id']) {
+                // carrega o registro
+                this.carregar(params['id']);
+            } else {
+                // prepara o form para uma nova inclusão
+                this.form.reset(this.novoRegistro);
+            }
+        });
+    }
 
     /**
      * @description Persiste as alteração da inclusão/edição
@@ -64,7 +92,7 @@ export abstract class CrudComponent<T> {
      * @description Executa a validação do form e atualiza o registro do banco
      * @param registro Registro que será atualizado
      */
-     private atualizar(registro: T): void {
+    private atualizar(registro: T): void {
         if (this.validarForm()) {
             this.loading = true;
             this.service.atualizar(registro).subscribe(() => {
@@ -91,12 +119,12 @@ export abstract class CrudComponent<T> {
             this.snackBar.open(error, 'Ok');
         });
     }
-    
+
     /**
      * @description Remove o registro do banco
      * @param registroId Id do registro a ser removido
      */
-     public remover(registroId: number): void {
+    public remover(registroId: number): void {
         this.loading = true;
         this.service.remover(registroId).subscribe(() => {
             this.loading = false;
